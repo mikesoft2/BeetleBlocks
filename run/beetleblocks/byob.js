52b98c12b07328f35a96a4b66f47e0cffa7903df
bromagosa <bromagosa@dosta.net>
2015-07-15 18:37:35 +0200

    fixes #115

--- run/beetleblocks/byob.js
+++ run/beetleblocks/byob.js
@@ -3,16 +3,40 @@ BlockDialogMorph.prototype.init = function (target, action, environment) {
     var myself = this;
     this.originalInit(target, action, environment);
     this.category = 'my blocks';
-    this.categories.children.forEach(function (each) {
-            each.refresh();
-            });
+    this.categories.children.forEach(function (each) { each.refresh() });
     if (this.types) {
         this.types.children.forEach(function (each) {
                 each.setColor(SpriteMorph.prototype.blockColor['my blocks']);
-                });
+            });
     }
     this.edit();
 }
+
+BlockEditorMorph.prototype.originalPopUp = BlockEditorMorph.prototype.popUp;
+BlockEditorMorph.prototype.popUp = function () {
+    var size = IDE_Morph.prototype.getSetting('editor-size'),
+        myself = this;
+
+    if (size) { 
+        var sizeArray = size.split(',');
+        this.setExtent(new Point(Number(sizeArray[0]), Number(sizeArray[1])));
+        this.drawNew()
+        this.fixLayout();
+    }
+
+    this.originalPopUp();
+
+    this.handle.originalMouseDownLeft = this.handle.mouseDownLeft;
+    this.handle.mouseDownLeft = function (pos) {
+        this.originalMouseDownLeft(pos);
+        this.originalStep = this.step;
+        this.step = function() {
+            this.originalStep();
+            IDE_Morph.prototype.saveSetting('editor-size', myself.extent().asArray());
+        }
+    }
+}
+
 
 BlockExportDialogMorph.prototype.exportBlocks = function () {
     var str = this.serializer.serialize(this.blocks),
