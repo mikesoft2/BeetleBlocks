4f7909658bdd15ba5fb2d49847c1579213ccdb04
bromagosa <bromagosa@e-citilab.eu>
2015-11-17 11:19:04 +0100

    more beetleCloud stuff

--- run/beetleblocks/cloud.js
+++ run/beetleblocks/cloud.js
@@ -1,28 +1,45 @@
 Cloud.prototype.originalCallService = Cloud.prototype.callService;
 Cloud.prototype.callService = function (
-    serviceName,
-    callBack,
-    errorCall,
-    args
-) {
+        serviceName,
+        callBack,
+        errorCall,
+        args
+        ) {
     this.originalCallService(serviceName, callBack, errorCall, args);
-    if (serviceName === 'publishProject') {
+    switch (serviceName) {
+        case 'publishProject':
+            postRequest(
+                    'project', 
+                    {
+                        projectName: args[0],
+                        username: this.username,
+                        thumbnail: args[1] // all publishProject calls need to send us the project thumbnail now
+                    });
+            return;
+        case 'deleteProject':
+        case 'unpublishProject':
+            postRequest(
+                    'delete-project',
+                    {
+                        projectName: args[0],
+                        username: this.username,
+                        thumbnail: '' // not needed
+                    });
+            return;
+    };
 
+    postRequest = function(entryPoint, data) {
         var request = new XMLHttpRequest();
-
         request.open(
-            'POST',
-            'http://snapp.citilab.eu:9999/project',
-            true
-        ); 
+                'POST',
+                'http://snapp.citilab.eu:9999/' + entryPoint,
+                true
+                ); 
         request.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
-        request.send(JSON.stringify({
-            projectName: args[0],
-            username: this.username,
-            thumbnail: args[1] // all publishProject calls need to send us the project thumbnail now
-        }));
-    }
-}
+        request.send(JSON.stringify(data));  
+    };
+
+};
 
 
 Cloud.prototype.urlForMyProject = function (projectName) {
@@ -33,9 +50,9 @@ Cloud.prototype.urlForMyProject = function (projectName) {
     }
 
     var id = 'Username=' + encodeURIComponent(this.username) + '&ProjectName=' + encodeURIComponent(projectName),
-        url = (this.hasProtocol() ? '' : 'http://')
-                + window.location.origin + window.location.pathname + '#present:'
-                + id;
+    url = (this.hasProtocol() ? '' : 'http://')
+        + window.location.origin + window.location.pathname + '#present:'
+        + id;
     return url;
-}
+};
 
